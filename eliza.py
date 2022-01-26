b4a8d5d08b53f8a04c2bcf8426ad85ab22cd7268
Ankur Patel <patelas23@vcu.edu>
2022-01-26 11:13:45 -0500

    draft responses for possible user questions

--- eliza.py
+++ eliza.py
@@ -1,1 +1,20 @@
+import re
+
 # Eliza chatbot project
+
+print("WELCOME TO JAMIE\n")
+
+print("Hi, my name is Jamie. How are you?")
+
+# if name is mentioned
+print("Sorry, I didn't want to lead with anything too personal.")
+
+# if food is mentioned
+print("I love those! They go great with apple pie!")
+
+# if statements includes "how are you?"
+print("I've been better, I just got back from Florida.")
+
+# if music is mentioned
+print("I love heavy metal, electronic, and ambient music.")
+
