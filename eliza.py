d4108347901622dbe7fb6b740fe9ecdec21014e2
Ankur Patel <patelas23@vcu.edu>
2022-02-02 18:01:55 -0500

    update objects from regex import

--- eliza.py
+++ eliza.py
@@ -2,6 +2,14 @@ import re
 
 # Eliza chatbot project
 
+userQuit = False
+currentResponse = 'Hello, my name is Jamie. My friends call me Eliza.'
+# Main loop
+while userQuit == False:
+    currentSentence = input(currentResponse)
+    
+
+
 print("WELCOME TO JAMIE\n")
 
 print("Hi, my name is Jamie. How are you?")
@@ -18,3 +26,9 @@ print("I've been better, I just got back from Florida.")
 # if music is mentioned
 print("I love heavy metal, electronic, and ambient music.")
 
+# TODO: Parsing function, extract and ranks keywords
+# Words following 'the', 'my', 'this' stored as nouns
+
+
+# TODO: Response generator, embed previously identified keywords
+# Consider more decomposition templates
