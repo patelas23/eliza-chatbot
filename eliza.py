007863a0abebd1ed6830187ab26b7fe52ae7f762
Ankur Patel <patelas23@vcu.edu>
2022-02-08 10:32:53 -0500

    conduct initial matches based on single keywords

--- eliza.py
+++ eliza.py
@@ -5,7 +5,7 @@ import re
 
 # Initialize Regex objects at top of tree
 key_word_pattern = re.compile(r'i|you|my|if')
-reflexiveSwitch = re.compile(r'(I)(am|don\'t|)(\w+)')
+reflexive_switch = re.compile(r'(I)(am|don\'t|)(\w+)')
 active_verb_pattern = re.compile(r'(\Bing)(\w+)')
 question_pattern = re.compile(r'(do you)(\w+)')
 # posessive_pattern = re.compile()
@@ -13,22 +13,29 @@ question_pattern = re.compile(r'(do you)(\w+)')
 # Create stack of possible responses
 response_stack = []
 
-userQuit = False
+user_quit = False
 current_response = 'Hello, my name is Jamie. My friends call me Eliza.'
 current_mutation = ''
+emotion_response = ''
 emotional_state_stack = []
 
 # Main loop
-while userQuit == False:
-    currentSentence = input(current_response + '\n')
+while user_quit == False:
+    current_sentence = input(current_response + '\n')
 
+    # If-else branches for most important templates
     # I am... -> Why are you...?
-    if(reflexiveSwitch.search(currentSentence) != None):
-        current_template = reflexiveSwitch.search(currentSentence)
+    keyword_match = key_word_pattern.search(current_sentence)
+    if(keyword_match):
+        print('pass')
+
+    reflexive_match = reflexive_switch.search(current_sentence)
+    if(reflexive_match):
+        current_template = reflexive_switch.search(current_sentence)
         current_mutation = 'Why are you ' + current_template.group(2)
         current_response = current_mutation
     # until... -> What will happen after...?
-    elif(active_verb_pattern.search(currentSentence)):
+    elif(active_verb_pattern.search(current_sentence)):
         pass
     # ... is so ... -> is ... always ...?
     elif():
@@ -38,8 +45,11 @@ while userQuit == False:
     # 
     else: # Pull response from available stack
         pass
+
+    # Add responses from if-else branches to weighted stack
+    # Randomly select a response from the stack
     
-    if(currentSentence == 'quit'):
+    if(current_sentence == 'quit'):
         break
 
     ### 'I can't' -> 'Why can't you?'
