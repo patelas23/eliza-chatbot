c824dfac7b43033c71dad58d0173ee527c570f83
Ankur Patel <patelas23@vcu.edu>
2022-02-08 12:05:13 -0500

    add groups to regex search patterns for sentence manipulation

--- eliza.py
+++ eliza.py
@@ -3,98 +3,78 @@ import re
 # Eliza chatbot project
 # Made with inspiration from https://web.njit.edu/~ronkowit/eliza.html
 
-# Initialize Regex objects at top of tree
+### Initialize Regex objects at top of tree
 key_word_pattern = re.compile(r'i|you|my|if')
-reflexive_switch = re.compile(r'(I)(am|don\'t|)(\w+)')
-active_verb_pattern = re.compile(r'(\Bing)(\w+)')
+reflexive_switch_pattern = re.compile(r'(\bi\b).+(\bam\b)\s(\w+)')
+active_verb_pattern = re.compile(r'(\w+ing)\s(\w+)')
 question_pattern = re.compile(r'(do you)(\w+)')
+exasperated_pattern = re.compile(r'(\w+)\s(is so)\s(\w+)')
+quit_pattern = re.compile(r'\bquit\b|\bbye\b')
 # posessive_pattern = re.compile()
 
-# Create stack of possible responses
+### Create stack of possible responses
 response_stack = []
 
+### Initialize variables for user interface
 user_quit = False
 current_response = 'Hello, my name is Jamie. My friends call me Eliza.'
-current_mutation = ''
-emotion_response = ''
-emotional_state_stack = []
 
-# Main loop
+### Main loop
 while user_quit == False:
+    ### Perform all parsing on lowercase input
     current_sentence = input(current_response + '\n')
+    current_sentence = current_sentence.lower()
 
-    # If-else branches for most important templates
-    # I am... -> Why are you...?
-    keyword_match = key_word_pattern.search(current_sentence)
-    if(keyword_match):
-        print('pass')
+    ### Parse sentence against each regex pattern
+    # keyword_match = key_word_pattern.search(current_sentence)
+    reflexive_match = reflexive_switch_pattern.search(current_sentence)
+    exasperated_match = exasperated_pattern.search(current_sentence)
+    quit_match = quit_pattern.search(current_sentence)
 
-    reflexive_match = reflexive_switch.search(current_sentence)
+    # if(keyword_match):
+    #     print('pass')
     if(reflexive_match):
-        current_template = reflexive_switch.search(current_sentence)
-        current_mutation = 'Why are you ' + current_template.group(2)
-        current_response = current_mutation
-    # until... -> What will happen after...?
-    elif(active_verb_pattern.search(current_sentence)):
+        current_response = 'Why are you ' + reflexive_match.group(3) + '?'
+    elif(exasperated_match):
+        current_response = "Is " + \
+            exasperated_match.group(1) + " always so " + \
+            exasperated_match.group(3) + "?"
+    else:
         pass
-    # ... is so ... -> is ... always ...?
-    elif():
-        pass
-    # do you... -> I could hope to ..., maybe someday
-    # 
-    # 
-    else: # Pull response from available stack
-        pass
+        # current_response = response_stack.pop()
 
     # Add responses from if-else branches to weighted stack
     # Randomly select a response from the stack
-    
-    if(current_sentence == 'quit'):
-        break
 
-    ### 'I can't' -> 'Why can't you?'
+    # if(quit_match):
+    #     user_quit = True
 
-    ### 'Today...' -> 'Does ... happen often?'
+    # 'I can't' -> 'Why can't you?'
 
-    ### 'feel' -> 'Is this a familiar feeling?
+    # 'Today...' -> 'Does ... happen often?'
 
-    ### TODO: Assign weights to each possible response
-
-    ### TODO: Randomize sub-responses
-
-    ### TODO: Construct stack of possible responses    
+    # 'feel' -> 'Is this a familiar feeling?
 
 
-print("WELCOME TO JAMIE\n")
+# print("WELCOME TO JAMIE\n")
 
-print("Hi, my name is Jamie. How are you?")
-
-# if name is mentioned
-print("Sorry, I didn't want to lead with anything too personal.")
-
-# if food is mentioned
-print("I love those! They go great with apple pie!")
-
-# if statements includes "how are you?"
-print("I've been better, I just got back from Florida.")
-
-# if music is mentioned
-print("I love heavy metal, electronic, and ambient music.")
-
-# if name is mentioned
-print("I don't really care about names.")
-
-# if pet, dog, or cat is mentioned
-print("I love taking care of ...s!")
-
-# if 'ha' with repetitions
-print("Why are you laughing?")
+# print("Hi, my name is Jamie. How are you?")
 
 
-# TODO: Parsing function, extract and ranks keywords
-# Words following 'the', 'my', 'this' stored as nouns
+# # if food is mentioned
+# print("I love those! They go great with apple pie!")
 
+# # if statements includes "how are you?"
+# print("I've been better, I just got back from Florida.")
 
-# TODO: Response generator, embed previously identified keywords
-# Consider more decomposition templates
+# # if music is mentioned
+# print("I love heavy metal, electronic, and ambient music.")
 
+# # if name is mentioned
+# print("I don't really care about names.")
+
+# # if pet, dog, or cat is mentioned
+# print("I love taking care of ...s!")
+
+# # if 'ha' with repetitions
+# print("Why are you laughing?")
